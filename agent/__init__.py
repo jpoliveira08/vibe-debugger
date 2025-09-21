5eedb96c6fbbc72425641da364f04364195b60ef
Joao Rocha <joaopedro.oliveirajava@gmail.com>
2025-09-21 12:59:09 -0300

    Restarting the project again

--- agent/__init__.py
+++
@@ -1,1 +0,0 @@
-# VibeDebugger Agent Package
--- agent/start-agent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start the LiteLLM proxy in the background
-litellm --config /usr/src/agent/litellm_config.yml &
-
-# Start the main Python application
-exec python main.py
